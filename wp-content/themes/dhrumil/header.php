45b542a1efff9d2a630447940a929d5342a31a19
codal-dkumbhani <dkumbhani@codal.com>
2021-02-23 17:39:46 +0530

    add some code

--- wp-content/themes/dhrumil/header.php
+++ wp-content/themes/dhrumil/header.php
@@ -27,6 +27,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.min.css">
 
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome.css">
+    
 
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/templatemo-lava.css">
 
